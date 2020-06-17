c854881fd8e62218499bc46e7a7b6441494510f6
Ulises Gomez <gomez_olguin@hotmail.com>
2020-06-17 09:45:48 -0500

    Initial Commit

---
+++ Planets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Planets
+//
+//  Created by Ulises Gomez Olguin on 17/06/20.
+//  Copyright © 2020 Ulises Gomez Olguin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
